51afe4d5e7c7d1c6a638af371da5ce74a9846a62
jinpulol <jesselehmonen@gmail.com>
2023-09-20 22:57:35 +0300

    3.1 Bookstore: Add JPA and database

--- src/main/java/hh/bookstore/BookstoreApplication.java
+++ src/main/java/hh/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package hh.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.bookstore.domain.Book;
+import hh.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+		return (args) -> {
+
+			log.info("Save a few books");
+			bookRepository.save(new Book("Why We Sleep: The New Science of Sleep and Dreams", "Matthew Walker", 2018,
+					"978-0141983769", 28.53));
+			bookRepository
+					.save(new Book("PREORDER The Last One", "Rachel Howzell Hall", 2024, "978-1649374400", 31.90));
+			bookRepository.save(new Book("Just Because", "Matthew McConaughey", 2023, "978-0593622032", 15.72));
+
+			log.info("Fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
--- src/main/java/hh/bookstore/domain/Book.java
+++ src/main/java/hh/bookstore/domain/Book.java
@@ -1,22 +1,25 @@
 package hh.bookstore.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
-    // attribuutit
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String title;
     private String author;
+    @Column(name = "published")
     private int year;
     private String isbn;
     private double price;
 
-    // konstruktorit
     public Book() {
-        super();
-        this.title = null;
-        this.author = null;
-        this.year = 0;
-        this.isbn = null;
-        this.price = 0;
     }
 
     public Book(String title, String author, int year, String isbn, double price) {
@@ -28,53 +31,57 @@ public class Book {
         this.price = price;
     }
 
-    // setterit
-    public void setTitle(String title) {
-        this.title = title;
+    public long getId() {
+        return id;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    // getterit
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public String getIsbn() {
         return isbn;
     }
 
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
     public double getPrice() {
         return price;
     }
 
-    // toString
-    @Override
-    public String toString() {
-        return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-                + "]";
+    public void setPrice(double price) {
+        this.price = price;
     }
 
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
 }
