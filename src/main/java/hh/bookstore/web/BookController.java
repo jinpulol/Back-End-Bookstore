c39daf8d2604f7f4016275f4370b1d25ca6f062e
jinpulol <jesselehmonen@gmail.com>
2023-09-21 01:23:27 +0300

    3.3 CRUD

--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.bookstore.domain.Book;
 import hh.bookstore.domain.BookRepository;
 
 @Controller
@@ -24,6 +27,27 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
 
         return "booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+
+        return "redirect:../booklist";
     }
 
 }
