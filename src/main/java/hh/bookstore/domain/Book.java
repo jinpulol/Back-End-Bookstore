818fd12ee1f20b4ef4564094ff648757486a92f5
jinpulol <jesselehmonen@gmail.com>
2023-10-05 01:08:22 +0300

    C5.1 added RESTful services

--- src/main/java/hh/bookstore/domain/Book.java
+++ src/main/java/hh/bookstore/domain/Book.java
@@ -1,5 +1,7 @@
 package hh.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,6 +25,7 @@ public class Book {
     private double price;
 
     @ManyToOne
+    @JsonIgnoreProperties("books")
     @JoinColumn(name = "categoryId")
     private Category category;
 
